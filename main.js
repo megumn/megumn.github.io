0bf48df1af5924a760361fac2f40b44546dcac29
megumn <mail@example.com>
2021-01-25 00:18:48 +0300

    add slider

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let webms = [];
 let startCounter = 0;
 let endCounter;
+let slider = document.createElement('input');
+let scontainer = document.getElementById('slidercontainer');
+let svalue = document.createElement('span');
 
 function playVideo(){
     let source = document.createElement('source');
@@ -36,6 +39,38 @@ function getWebms(response){
             if(posts[i]["files"][k]["path"].match(/.webm/i) || posts[i]["files"][k]["path"].match(/.mp4/i))
                 webms[j++] = 'https://2ch.hk' + posts[i]["files"][k]["path"];
     endCounter = webms.length-1;
+
+
+    slider.setAttribute('type', 'range');
+    slider.setAttribute('min', 0);
+    slider.setAttribute('max', webms.length-1);
+    slider.setAttribute('id', 'slider');
+    scontainer.appendChild(slider);
+
+    svalue.setAttribute('id', 'slidervalue');
+    scontainer.appendChild(svalue);
+
+    if(document.getElementsByName('position')[0].checked) {
+        slider.setAttribute('value', 0);
+        slider.value = 0
+        svalue.innerHTML = 0;
+    }else{
+        slider.setAttribute('value', webms.length-1);
+        slider.value = webms.length-1;
+        svalue.innerHTML = webms.length-1;
+    }
+
+    slider.onchange = function() {
+        slider.setAttribute('value', this.value);
+        slider.value = this.value
+        svalue.innerHTML = this.value;
+        if(document.getElementsByName('position')[0].checked)
+            startCounter = this.value;
+        else
+            endCounter = this.value;
+        playVideo();
+    }
+
     playVideo();
 }
 
@@ -63,19 +98,37 @@ function process(){
 }
 
 function nextVideo(){
-    if(document.getElementsByName('position')[0].checked && startCounter < webms.length-1)
+    let svalue = document.getElementById('slidervalue');
+    let slider = document.getElementById('slider');
+    if(document.getElementsByName('position')[0].checked && startCounter < webms.length-1){
         ++startCounter;
-    else if(endCounter > 0)
+        slider.setAttribute('value', startCounter);
+        slider.value = startCounter;
+        svalue.innerHTML = startCounter;
+    }else if(endCounter > 0){
         --endCounter;
+        slider.setAttribute('value', endCounter);
+        slider.value = endCounter;
+        svalue.innerHTML = endCounter;
+    }
 
     playVideo();
 }
 
 function prevVideo(){
-    if(document.getElementsByName('position')[0].checked && startCounter > 0)
+    let svalue = document.getElementById('slidervalue');
+    let slider = document.getElementById('slider');
+    if(document.getElementsByName('position')[0].checked && startCounter > 0){
         --startCounter;
-    else if(endCounter < webms.length-1)
+        slider.setAttribute('value', startCounter);
+        slider.value = startCounter;
+        svalue.innerHTML = startCounter;
+    }else if(endCounter < webms.length-1){
         ++endCounter;
+        slider.setAttribute('value', endCounter);
+        slider.value = endCounter;
+        svalue.innerHTML = endCounter;
+    }
 
     playVideo();
 }
